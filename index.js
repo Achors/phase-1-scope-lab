10a08c654e56b6ffb9902313a8e05ee18d62217f
Adolf <adolfnyerere@gmail.com>
2023-09-28 22:48:32 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = "bob";
+// console.log(customerName);
+
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase();
+}
+
+var bestCustomer;
+
+function setBestCustomer(){
+   return bestCustomer = "not bob";
+}
+function overwriteBestCustomer(){
+   return bestCustomer = "maybe bob";
+}
+const leastFavoriteCustomer = "Adolf";
+function changeLeastFavoriteCustomer(){
+    return leastFavoriteCustomer = "Nyerere";
+ }
